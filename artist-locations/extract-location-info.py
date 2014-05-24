a732bd48985163ae1d4ef4886a0853d5f1c66515
Uwe L. Korn <uwelk@xhochy.com>
2014-05-24 20:31:10 +0200

    Parse location types too

--- artist-locations/extract-location-info.py
+++ artist-locations/extract-location-info.py
@@ -18,6 +18,9 @@ except ImportError:
 type_name_relation = re.compile(r"<http:\/\/rdf.freebase.com\/ns\/m\.([^>]*)>\s+<http:\/\/rdf.freebase.com\/ns\/type.object.name>\s+\"(.*)")
 # Matches a location-location relation, both groups are locations
 containedby_relation = re.compile(r'<http:\/\/rdf.freebase.com\/ns\/m\.([^>]*)>\s+<http:\/\/rdf.freebase.com\/ns\/location.location.containedby>\s+<http:\/\/rdf.freebase.com\/ns\/m\.([^>]*)>')
+# Matches an object-type relation, first group is the object id, second the type
+# <http://rdf.freebase.com/ns/m.04jpl>    <http://rdf.freebase.com/ns/type.object.type>   <http://rdf.freebase.com/ns/rail.railway_terminus>      .
+type_relation = re.compile(r'<http:\/\/rdf.freebase.com\/ns\/m\.([^>]*)>\s+<http:\/\/rdf.freebase.com\/ns\/type.object.type>\s+<http:\/\/rdf.freebase.com\/ns\/([^>]*)>')
 
 dump_file = sys.argv[1]
 ids_file = sys.argv[2]
@@ -31,7 +34,7 @@ with gzip.open(ids_file, 'rt') as idsf:
     for id in idsf:
         # Be careful to always stip the newline character at the end
         ids.add(id.strip())
-        locations[id.strip()] = { 'contained_by': [], 'names': {} }
+        locations[id.strip()] = { 'contained_by': [], 'names': {}, 'types': [] }
 
 # Read the freebase dump line-by-line and match on all known location relations
 with gzip.open(dump_file, 'rt') as freebase:
@@ -53,6 +56,12 @@ with gzip.open(dump_file, 'rt') as freebase:
             if id in ids:
                 locations[id]['contained_by'].append(m.group(2))
             continue
+        m = type_relation.match(row)
+        if m:
+            id = m.group(1)
+            if id in ids:
+                locations[id]['types'].append(m.group(2))
+            continue
 
 
 for id in locations.keys():
@@ -68,5 +77,5 @@ for id in locations.keys():
     del locations[id]['names']
 
 with gzip.open(locations_file, 'wt') as f:
-    f.write(dump(locations, Dumper=Dumper))
+    dump(locations, f, Dumper=Dumper)
 
