373318b1619e7c183b8839c1772c5ff69107dfe4
Uwe L. Korn <uwelk@xhochy.com>
2014-09-21 19:58:22 +0200

    Add simple MusicBrainz artist lister

---
+++ musicbrainz/src/main/java/com/xhochy/musicbrainz/ArtistNames.java
@@ -0,0 +1,39 @@
+package com.xhochy.musicbrainz;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ArtistNames
+{
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+            private final static IntWritable one = new IntWritable(1);
+            private Text word = new Text();
+
+            public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+                word.set(value.toString().split("\t")[2]);
+                context.write(word, one);
+            }
+    }
+    
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+}
---
+++ musicbrainz/src/main/java/com/xhochy/musicbrainz/Main.java
@@ -0,0 +1,42 @@
+package com.xhochy.musicbrainz;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Main {
+
+    /**
+     * Selector method to instantiate the correct job.
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.err.println("You need to specify at leat the job name!");
+            System.exit(1);
+        }
+
+        Configuration conf = new Configuration();
+
+        String jobname = args[0];
+        Job job = Job.getInstance(conf, jobname);
+
+        if (jobname.equals("artistnames")) {
+            job.setJarByClass(ArtistNames.class);
+            job.setMapperClass(ArtistNames.TokenizerMapper.class);
+            job.setCombinerClass(ArtistNames.IntSumReducer.class);
+            job.setReducerClass(ArtistNames.IntSumReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+            FileInputFormat.addInputPath(job, new Path(args[1]));
+            FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        } else {
+            System.err.println("Jobname not found!");
+            System.exit(1);
+        }
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
